2b37b6e7c995c64b1dbe4dd8ff2f7a28df3a6dac
Omar-Vayani01~ <vayaniumar@gmail.com>
2022-02-16 23:22:55 +0200

    changed the box-shadow

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Song = ({ currentSong, isPlaying }) => {
+  const s = {
+    boxShadow: `0px 0px 60px ${currentSong.color[0]}, 0px 0px 20px ${currentSong.color[1]}`,
+  };
   return (
     <div className="song-container">
       <img
         className={isPlaying ? "active" : "continue"}
+        style={s}
         src={currentSong.cover}
         alt={`Album: ${currentSong.name}}`}
       />
